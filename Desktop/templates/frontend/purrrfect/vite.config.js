d3273501dbbfb95fc7463c6cc94c7a726dcc4b47
aya.gallarte <aya.gallarte@gmail.com>
2024-08-03 06:55:59 +0800

    modified project

--- Desktop/templates/frontend/purrrfect/vite.config.js
+++ Desktop/templates/frontend/purrrfect/vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/purrrfect/', 
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/purrrfect/', 
-})
+  base: '/purrrfect/', // Ensure this matches your repository name
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
+});
