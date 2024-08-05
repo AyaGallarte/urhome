9262bacd8c9fd516bc8826a82af94bc29e6a8058
aya.gallarte <aya.gallarte@gmail.com>
2024-08-06 07:10:52 +0800

    modified vite config

--- Desktop/templates/frontend/fitnessApp-client/vite.config.js
+++ Desktop/templates/frontend/fitnessApp-client/vite.config.js
@@ -1,20 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/purrrfect/', 
-// })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fitnessApp-client/', // Ensure this matches your repository name
+  base: '/', 
   build: {
-    rollupOptions: {
-      input: '/index.html',
-    },
+
   },
 });
