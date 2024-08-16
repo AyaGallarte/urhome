697795e9b6784660311e1219a178592438fbc6e5
aya.gallarte <aya.gallarte@gmail.com>
2024-08-16 08:13:12 +0800

    modified App.jsx

--- Desktop/ecommerce-app/src/App.jsx
+++ Desktop/ecommerce-app/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   }
 
   const token = localStorage.getItem('token');
-//https://ecommerce-api-9r0z.onrender.com
+
   useEffect(() => {
      if (token !== null){
           fetch('https://ecommerce-api-9r0z.onrender.com/users/details', {
@@ -38,7 +38,7 @@ function App() {
     })
     .then(res => res.json())
     .then(data => {
-      if(typeof data !== 'undefined') {
+      if(typeof data !== undefined) {
         setUser({
           id: data.user._id,
           isAdmin: data.user.isAdmin
@@ -83,4 +83,91 @@ function App() {
 }
 
 export default App;
+
+
+// import { useState, useEffect } from 'react';
+// import { Container } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { UserProvider } from './context/UserContext';
+// import AppNavbar from './components/AppNavbar';
+
+// import Home from './pages/Home';
+// import Profile from './pages/Profile';
+// import Register from './pages/Register';
+// import Login from './pages/Login';
+// import Logout from './pages/Logout';
+// import ProductsCatalog from './pages/ProductsCatalog'
+// import ProductView from './pages/ProductView'
+// import AddProduct from './pages/AddProduct'
+// import CartView from './pages/CartView'
+// import OrderView from './pages/OrderView'
+// import AdminOrderView from './pages/AdminOrderView'
+
+// function App() {
+//   const [user, setUser] = useState({
+//     id: null,
+//     isAdmin: null
+//   });
+
+//   function unsetUser() {
+//     localStorage.clear();
+//   }
+
+//   const token = localStorage.getItem('token');
+// //https://ecommerce-api-9r0z.onrender.com
+//   useEffect(() => {
+//      if (token !== null){
+//           fetch('https://ecommerce-api-9r0z.onrender.com/users/details', {
+//       headers: {
+//         Authorization: `Bearer ${localStorage.getItem('token')}`
+//       }
+//     })
+//     .then(res => res.json())
+//     .then(data => {
+//       if(typeof data !== 'undefined') {
+//         setUser({
+//           id: data.user._id,
+//           isAdmin: data.user.isAdmin
+//         });
+//       } else {
+//         setUser({
+//           id: null,
+//           isAdmin: null
+//         });
+//       }
+//     });
+//      }
+//   }, []);
+
+//   useEffect(() => {
+//     console.log(user);
+//     console.log(localStorage);
+//   }, [user]);
+
+//   return (
+//     <UserProvider value={{ user, setUser, unsetUser }}>
+//       <Router>
+//         <AppNavbar />
+//         <Container>
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/profile" element={<Profile />} />
+//             <Route path="/register" element={<Register />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route path="/logout" element={<Logout />} />
+//             <Route path="/products" element={<ProductsCatalog />}/>
+//             <Route path="/products/:productId" element={<ProductView />}/>
+//             <Route path="/addProduct" element={<AddProduct />}/>
+//             <Route path="/cart" element={<CartView />}/>
+//             <Route path="/order" element={<OrderView />}/>
+//             <Route path="/adminOrder" element={<AdminOrderView />}/>
+//           </Routes>
+//         </Container>
+//       </Router>
+//     </UserProvider>
+//   );
+// }
+
+// export default App;
 
